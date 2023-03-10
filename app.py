b03aeb707165518f4f987e75b7e589fd3a91ca65
chase <freewayrong@foxmail.com>
2023-03-11 01:44:36 +0800

    add two select form

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ def index_html():
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
+    default_language = 'Chinese'  # 默认语言为中文
+    default_option = 'transcribe'  # 默认选项为 transcribe
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -28,23 +30,33 @@ def upload_file():
         # Save the file to disk
         file_path = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(file_path)
-        # Use whisper to transcribe the file
-        cmd = ['/home/chase/Documents/miniconda3/envs/whisper/bin/python',
-               '/home/chase/Documents/miniconda3/envs/whisper/bin/whisper', file_path, '--model',
-               'medium', '--language', 'Chinese', '-f', 'txt']
+        # Use whisper to transcribe/translate the file based on the selected option
+        option = request.form.get('option', default_option)  # 获取用户选择的选项，默认为 transcribe
+        if option == 'transcribe':
+            cmd = ['/home/chase/Documents/miniconda3/envs/whisper/bin/python',
+                   '/home/chase/Documents/miniconda3/envs/whisper/bin/whisper', file_path, '--model',
+                   'medium', '--language', default_language, '-f', 'txt']
+        elif option == 'translate':
+            cmd = ['/home/chase/Documents/miniconda3/envs/whisper/bin/python',
+                   '/home/chase/Documents/miniconda3/envs/whisper/bin/whisper', file_path, '--model',
+                   'medium', '--src-lang', default_language, '--trg-lang', 'en', '-f', 'txt']
+        else:
+            return jsonify({'error': 'Invalid option selected.'}), 400
         proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
         stdout, stderr = proc.communicate()
         stdout_zh_cn = zhconv.convert(stdout.decode("utf-8"), 'zh-cn')
         if stderr:
             return jsonify({'error': stderr.decode('utf-8')}), 400
         else:
-            transcription = stdout_zh_cn
+            if option == 'transcribe':
+                result = stdout_zh_cn.replace('\n', '<br>')  # 替换为HTML换行标签
+            else:
+                result = stdout_zh_cn
             # Remove the uploaded file from disk
             os.remove(file_path)
-            return jsonify({'transcription': transcription})
+            return jsonify({'result': result})
     else:
         return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
