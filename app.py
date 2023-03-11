7a65b9ec2b5300da6c77fba49fa0cecb23c56ee3
chase <freewayrong@foxmail.com>
2023-03-11 14:27:48 +0800

    add video transcribe

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import imghdr
 import zhconv
 from flask import Flask, request, jsonify, render_template, Response
 from flask_cors import CORS
@@ -18,12 +19,13 @@ def index_html():
 def run_whisper(cmd):
     proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
     for line in iter(proc.stdout.readline, b''):
-        yield f'{zhconv.convert(line.decode("utf-8"), "zh-cn")}\n'
+        yield f'{zhconv.convert(line.decode("utf-8"), "zh-cn")}'
     for line in iter(proc.stderr.readline, b''):
         yield f'{line.decode("utf-8")}\n'
     proc.stdout.close()
     proc.stderr.close()
     proc.wait()
+
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -41,6 +43,18 @@ def upload_file():
         # Save the file to disk
         file_path = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(file_path)
+
+        # Convert video to audio if file is a video
+        file_type = imghdr.what(file_path)
+        if file_type in ('mp4', 'avi', 'mov', 'mkv'):
+            audio_path = os.path.splitext(file_path)[0] + '.wav'
+            cmd = ['ffmpeg', '-i', file_path, '-vn', '-acodec', 'pcm_s16le', '-ar', '16000', audio_path]
+            proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
+            proc.communicate()
+            if proc.returncode != 0:
+                return jsonify({'error': 'Failed to convert video to audio.'}), 400
+            file_path = audio_path
+
         # Use whisper to transcribe/translate the file based on the selected option and model
         option = request.form.get('option', default_option)  # 获取用户选择的选项，默认为 transcribe
         model = request.form.get('model', default_model)  # 获取用户选择的模型，默认为 medium
@@ -54,10 +68,13 @@ def upload_file():
                    model, '--src-lang', default_language, '--trg-lang', 'en', '-f', 'txt']
         else:
             return jsonify({'error': 'Invalid option selected.'}), 400
+        # Run whisper
+
         # Run whisper command and return output using SSE
         return Response(run_whisper(cmd), mimetype='text/event-stream')
     else:
         return render_template('upload.html')
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
